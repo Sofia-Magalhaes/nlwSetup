f484ba31ea01a1bf6131907e00f1e14a5eb34bf5
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2023-01-17 17:18:59 -0300

    Configuração MOBILE finalizada

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -26,7 +26,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>
-        Open up App.tsx to start working on your app!
+        Open up App.tsx to start working on your app!!
       </Text>
       <StatusBar
         barStyle={"light-content"}
