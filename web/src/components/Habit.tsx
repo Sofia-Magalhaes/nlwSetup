5558386d6f23a0718a48a0ca0479490229285df2
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2023-01-17 16:09:20 -0300

    Configuração da WEB finalizado

--- web/src/components/Habit.tsx
+++ web/src/components/Habit.tsx
@@ -4,7 +4,7 @@ interface HabitProps {
 
 export function Habit(props: HabitProps) {
   return (
-    <div className="bg-zinc-900 w-10 h-10 text-white rounded m-2 flex items-center ">
+    <div className="bg-zinc-900 w-10 h-10 text-white rounded m-2 flex items-center justify-center ">
       {props.completed}
     </div>
   );
