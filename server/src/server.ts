07d337ad1d765bdb506ecc8eae4663f2df9d3a39
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2023-01-17 15:34:51 -0300

    configurando prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,17 @@
 import Fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
 
 const app = Fastify()
+const prisma = new PrismaClient()
 
-app.get('/', () => {
-    return 'teste'
+app.get('/hello', async () => {
+    const habits = await prisma.habit.findMany()
+
+    return habits
 })
 
 app.listen({
     port:3333,
+}).then(() => {
+    console.log('HTTP Server Running')
 })
