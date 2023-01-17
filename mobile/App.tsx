0ced4bf28fb051915894076ca00278248c73c185
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2023-01-17 17:14:56 -0300

    fontes e componente de loading
    instalando as fontes, carregamento das fontes e criando o componente do Loading

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,23 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+} from "@expo-google-fonts/inter";
+
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  });
+
+  // esse if faz com que se a fonte não estiver carregada o app não vai entrar direto
+  // vai exibir uma tala de carregamento 
+  if (!fontsLoaded) {
+    return(
+      <Loading />
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>
+        Open up App.tsx to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {    
+  container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#09090A",
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
-    color: 'red',
+    color: "#fff",
+    fontFamily: 'Inter_800ExtraBold'
   },
 });
