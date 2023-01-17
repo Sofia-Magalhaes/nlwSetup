1c72cbc1eae2fbc5cc1ea02a5113f08c38124305
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2023-01-17 15:36:41 -0300

    configurando CORS

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,11 @@
 import Fastify from 'fastify';
+import cors from '@fastify/cors'
 import { PrismaClient } from '@prisma/client';
 
 const app = Fastify()
 const prisma = new PrismaClient()
+
+app.register(cors)
 
 app.get('/hello', async () => {
     const habits = await prisma.habit.findMany()
@@ -11,7 +14,7 @@ app.get('/hello', async () => {
 })
 
 app.listen({
-    port:3333,
+    port: 3333,
 }).then(() => {
     console.log('HTTP Server Running')
 })
