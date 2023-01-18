7b753e44e347e097d363de77fb5c7544fd3150f8
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2023-01-17 21:42:45 -0300

    Rota de Criação de novo hábito

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,3 +1,5 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+export const prisma = new PrismaClient({
+  log: ['query'],
+})
