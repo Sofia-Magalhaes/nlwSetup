1be495663d103092a2941c86cbee34532c170e42
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2023-01-21 00:54:54 -0300

    animação da progress bar e checkbox

--- mobile/src/components/Checkbox.tsx
+++ mobile/src/components/Checkbox.tsx
@@ -1,8 +1,14 @@
-import { TouchableOpacity, View, Text, TouchableOpacityProps } from "react-native";
+import {
+  TouchableOpacity,
+  View,
+  Text,
+  TouchableOpacityProps,
+} from "react-native";
 import { Feather } from "@expo/vector-icons";
 import colors from "tailwindcss/colors";
+import Animated, { ZoomIn, ZoomOut} from "react-native-reanimated";
 
-interface Props extends TouchableOpacityProps{
+interface Props extends TouchableOpacityProps {
   title: string;
   checked?: boolean;
 }
@@ -14,13 +20,17 @@ export function Checkbox({ checked = false, title, ...rest }: Props) {
       className="flex-row mb-2 items-center"
       {...rest}
     >
-      {checked ? 
-        <View className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center">
+      {checked ? (
+        <Animated.View
+          className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center"
+          entering={ZoomIn}
+          exiting={ZoomOut}
+        >
           <Feather name="check" size={20} color={colors.white} />
-        </View>
-       : 
+        </Animated.View>
+      ) : (
         <View className="h-8 w-8 bg-zinc-900 rounded-lg" />
-      }
+      )}
 
       <Text className="text-white text-base ml-3 font-semibold">{title}</Text>
     </TouchableOpacity>
--- mobile/src/components/ProgressBar.tsx
+++ mobile/src/components/ProgressBar.tsx
@@ -1,5 +1,6 @@
+import { useEffect } from "react";
 import { View } from "react-native";
-import Animated,  {useAnimatedStyle, useSharedValue} from "react-native-reanimated";
+import Animated,  {useAnimatedStyle, useSharedValue, withTiming, withDelay} from "react-native-reanimated";
 
 interface Props {
   progress?: number;
@@ -13,6 +14,11 @@ export function ProgressBar({ progress = 0 }: Props) {
       width: `${sharedProgress.value}%`
     };
   });
+
+  useEffect(() => {
+    sharedProgress.value = withDelay(200, withTiming(progress));
+  }, [progress]);
+  
   return (
     <View className="w-full h-3 rounded-xl bg-zinc-700 mt-4">
       <Animated.View
