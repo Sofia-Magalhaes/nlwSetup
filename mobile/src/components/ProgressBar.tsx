2ecc10ee9f98537496fffb4d644f45ec3e637a59
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2023-01-21 00:31:49 -0300

    ajustes

--- mobile/src/components/ProgressBar.tsx
+++ mobile/src/components/ProgressBar.tsx
@@ -1,15 +1,23 @@
 import { View } from "react-native";
+import Animated,  {useAnimatedStyle, useSharedValue} from "react-native-reanimated";
 
 interface Props {
   progress?: number;
 }
 
 export function ProgressBar({ progress = 0 }: Props) {
+  const sharedProgress = useSharedValue(progress);
+
+  const style = useAnimatedStyle(() => {
+    return{
+      width: `${sharedProgress.value}%`
+    };
+  });
   return (
     <View className="w-full h-3 rounded-xl bg-zinc-700 mt-4">
-      <View
+      <Animated.View
         className="h-3 rounded-xl bg-violet-600"
-        style={{ width: `${progress}%` }}
+        style={style}
       />
     </View>
   );
